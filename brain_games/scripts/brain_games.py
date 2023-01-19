cd881bc463c9b8f615d42ea7d00ac0e4f2376e14
Victoria Treskova <vikatresk@yandex.ru>
2023-01-19 17:59:38 +0500

    brain-games script corrected

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,9 +3,8 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
     welcome_user()
-
 
 
 if __name__ == '__main__':
