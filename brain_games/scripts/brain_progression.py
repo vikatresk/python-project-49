12d47a542abe10201e87581c8457110bd2981131
Victoria Treskova <vikatresk@yandex.ru>
2023-01-19 18:00:39 +0500

    brain-progression script corrected

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from brain_games.games import brain_progression
 from brain_games import game_engine
 
@@ -9,3 +9,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
