a93b670a164b2e3513a4324a21d57d3fa0bce29b
Victoria Treskova <vikatresk@yandex.ru>
2023-01-19 17:18:34 +0500

    brain-even flake corrected

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,10 +1,12 @@
 import random
 
+
 GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
-MAX_NUMBER = 200 #максимальное число
+MAX_NUMBER = 200  # максимальное число
+
 
 def play():
-    number = random.randint(0, MAX_NUMBER)
+    number = random.randint (0, MAX_NUMBER)
     question = f'{number}'
     if number%2 == 0:
         output = 'yes'
