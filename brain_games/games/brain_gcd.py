0d42cefd3a97df99df4037eb2aac05d05efeac09
Victoria Treskova <vikatresk@yandex.ru>
2023-01-19 17:55:55 +0500

    brain-gcd after flake corrected

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,8 +1,10 @@
 import random
 import math
 
+
 GAME = 'Find the greatest common divisor of given numbers.'
 MAX_NUMBER = 100
+
 
 def play():
     number = random.randint(1, MAX_NUMBER)
@@ -10,3 +12,4 @@ def play():
     question = f'{number} {number1}'
     output = math.gcd(number, number1)
     return question, output
+    
