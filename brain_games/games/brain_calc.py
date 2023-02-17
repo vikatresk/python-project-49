f31f965a3d5124a8a768a32ef709bda14ba84240
Victoria Treskova <vikatresk@yandex.ru>
2023-02-17 13:33:34 +0500

    brain_calc again

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,12 +7,12 @@ MAX_NUMBER = 15
 
 def play_round():
     number1 = random.randint(0, MAX_NUMBER)
-    operator = random.choice(['+','-', '*'])
+    operator = random.choice(['+','-','*'])
     number2 = random.randint(0, MAX_NUMBER)
     question = f'{number1 } {operator} { number2}'
-    return question, connumerate(number1, number2, operator)
+    return question, calculate(number1, number2, operator)
     
-def connumerate(number1, number2, operator):
+def calculate(number1, number2, operator):
     if operator == '+':
         result = number1 + number2
     if operator == '-':
