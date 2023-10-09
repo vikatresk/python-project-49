7a294b16e3ac89d9d5342b1f06ad6305990d79b5
Victoria Treskova <vikatresk@yandex.ru>
2023-10-09 13:54:36 +0500

    fixing the typo

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -16,6 +16,6 @@ def is_prime(number):
     if number <= 1:
         return False
     for x in range(2, int(number ** 0.5 + 1)):
-        if number % i == 0:
+        if number % x == 0:
             return False
     return True
